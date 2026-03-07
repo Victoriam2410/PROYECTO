ab5d5478a8f714041606bb134b63fc7de7174160
Victoriam2410 <estherrodas61@gmail.com>
2026-03-07 00:32:36 -0600

    Case 1 terminado

--- PROYECTO/Program.cs
+++ PROYECTO/Program.cs
@@ -7,24 +7,49 @@
         Console.WriteLine("Bienvenido al menú princilap de Vick´s Prime");
         Console.WriteLine("Cual de las opciones desea elejir?");
         Console.WriteLine("\n1.Evaluar Contenido\n 2.Politicas\n 3.Estadisticas\n 4.Reiniciar Estadisticas\n 5.Salis.");
+        option = int.Parse(Console.ReadLine());
         switch (option)
         {
             case 1:
-
+                pedirDatos();
                 break;
             case 2:
-
                 break;
             case 3:
-
                 break;
             case 4:
-
                 break;
             case 5:
-
                 break;
         }
-    } while (option != 0);
-    }
+
+        void pedirDatos()
+        {
+            Console.WriteLine("Ingrese el tipo de contenido (1.película, 2.serie, 3.documental, 4.evento en vivo):");
+            int tipo = int.Parse(Console.ReadLine());
+
+            switch (tipo)
+            {
+                case 1:
+                    Console.WriteLine("ingrese el nombre de la pelicula");
+                    string nombrePelicula = Console.ReadLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Ingrese el nombre de la Serie");
+                    string nombreSerie = Console.ReadLine();
+                    break;
+                case 3:
+                    Console.WriteLine("Ingrese el nombre del Documental");
+                    string nombreDocumental = Console.ReadLine();
+                    break;
+                case 4:
+                    Console.WriteLine("Ingrese el nombre del evento en vivo");
+                    string nombreVivo = Console.ReadLine();
+                    break;
+                default:
+                    break;
+            }
+        }
+    } while (option != 5);
+}
 menuPrincipal();
