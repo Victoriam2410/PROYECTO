3e502b963156e15ccac50b651b07dce9a62b92a6
Victoriam2410 <estherrodas61@gmail.com>
2026-03-06 23:44:26 -0600

    Creacion del menú principal

--- PROYECTO/Program.cs
+++ PROYECTO/Program.cs
@@ -1,4 +1,30 @@
 ﻿void menuPrincipal()
 {
+    int option = 0;
+    do
+    {
+        Console.WriteLine("   Menú Principal Vick´s Prime   ");
+        Console.WriteLine("Bienvenido al menú princilap de Vick´s Prime");
+        Console.WriteLine("Cual de las opciones desea elejir?");
+        Console.WriteLine("\n1.Evaluar Contenido\n 2.Politicas\n 3.Estadisticas\n 4.Reiniciar Estadisticas\n 5.Salis.");
+        switch (option)
+        {
+            case 1:
 
-}
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+        }
+    } while (option != 0);
+    }
+menuPrincipal();
