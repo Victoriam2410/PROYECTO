6fce0f60b7677ef02c1f95a273031adfce5d37b6
Victoriam2410 <estherrodas61@gmail.com>
2026-03-09 23:38:37 -0600

    Se reubicaron los if, y se agregaron returns para detener el programa cuando se ingresa un dato que no cumple con las condiciones

--- PROYECTO/Program.cs
+++ PROYECTO/Program.cs
@@ -30,6 +30,7 @@
             bool cumpleDuracion = false;
             bool cumpleHora=false;
             bool cumpleProduccion=true;
+
             switch (tipo)
             {
                 case 1:
@@ -37,128 +38,173 @@
                     string nombrePelicula = Console.ReadLine();
                     Console.WriteLine("Cual es la duración de la pelicula?");
                     int duracionPeli = int.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la clasificación ( 1.- todo público, 2.- +13, 3.- +18 )");
-                    int clasificacionUNO = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingrese la hora programada (0-23):");
-                    int horaUno = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
-                    int produccionUno = int.Parse(Console.ReadLine());
                     if (duracionPeli >= 60 && duracionPeli <= 180)
                     {
                         cumpleDuracion = true;
                     }
+                    else
+                    {
+                        Console.WriteLine("La duracion de la pelicula no cumple con el reglamento.\n");
+                        return;
+                    }
+                    Console.WriteLine("ingrese la clasificación ( 1.- todo público, 2.- +13, 3.- +18 )");
+                    int clasificacionUNO = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la hora programada (0-23):");
+                    int horaUno = int.Parse(Console.ReadLine());
                     if (clasificacionUNO == 1)
-                    { 
+                    {
                         cumpleHora = true;
                     }
-                    if (clasificacionUNO == 2 && horaUno >= 6 && horaUno <= 22)
+                    else if (clasificacionUNO == 2 && horaUno >= 6 && horaUno <= 22)
                     {
-                        cumpleHora=true;
+                        cumpleHora = true;
                     }
-                    if (clasificacionUNO == 3 && horaUno >= 22 || horaUno <= 5)
+                    else if (clasificacionUNO == 3 && horaUno >= 22 || horaUno <= 5)
                     {
-                        cumpleHora=true;
+                        cumpleHora = true;
                     }
+                    else
+                    {
+                        Console.WriteLine("El horario no es apto para la clasificación.\n");
+                        return;
+                    }
+
+                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
+                    int produccionUno = int.Parse(Console.ReadLine());
                     if (produccionUno == 1 && clasificacionUNO == 3)
                     {
-                        cumpleProduccion = false;
+                        Console.WriteLine("Producción baja no permitida para +18\n");
+                        return;
                     }
-                        break;
+                    break;
                 case 2:
                     Console.WriteLine("Ingrese el nombre de la Serie");
                     string nombreSerie = Console.ReadLine();
                     Console.WriteLine("Cual es la duración de la Serie?");
                     int duracionSerie = int.Parse(Console.ReadLine());
+                    if (duracionSerie >= 20 && duracionSerie <= 90)
+                    {
+                        cumpleDuracion = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("La duracion de la serie no cumple con el reglamento.\n");
+                        return;
+                    }
                     Console.WriteLine("ingrese la clasificación (1.Todo publico, 2.+13, 3.+18)");
                     int clasificacionDOS = int.Parse(Console.ReadLine());
                     Console.WriteLine("Ingrese la hora programada:");
                     int horaDos = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
-                    int produccionDos = int.Parse(Console.ReadLine());
-                    if (duracionSerie >= 20 && duracionSerie <=90)
-                    {
-                        cumpleDuracion = true;
-                    }
                     if (clasificacionDOS == 1)
                     {
-                        cumpleHora=true;
+                        cumpleHora = true;
                     }
-                    if (clasificacionDOS == 2 && horaDos >= 6 && horaDos <= 22)
+                    else if (clasificacionDOS == 2 && horaDos >= 6 && horaDos <= 22)
                     {
                         cumpleHora = true;
                     }
-                    if (clasificacionDOS == 3 && horaDos >= 22 || horaDos <= 5)
+                    else if (clasificacionDOS == 3 && horaDos >= 22 || horaDos <= 5)
                     {
                         cumpleHora = true;
                     }
+                    else
+                    {
+                        Console.WriteLine("El horario no es apto para la clasificación.\n");
+                        return;
+                    }
+                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
+                    int produccionDos = int.Parse(Console.ReadLine());
                     if (produccionDos == 1 && clasificacionDOS == 3)
                     {
-                        cumpleProduccion = false;
+                        Console.WriteLine("Producción baja no permitida para +18\n");
+                        return;
                     }
-
                     break;
                 case 3:
                     Console.WriteLine("Ingrese el nombre del Documental");
                     string nombreDocumental = Console.ReadLine();
                     Console.WriteLine("Cual es la duración del Documental?");
                     int duracionDocu = int.Parse(Console.ReadLine());
+                    if (duracionDocu >= 30 && duracionDocu <= 120)
+                    {
+                        cumpleDuracion = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("La duracion del documental no cumple con el reglamento.\n");
+                        return;
+                    }
                     Console.WriteLine("ingrese la clasificación (1.Todo publico, 2.+13, 3.+18)");
                     int clasificacionTres = int.Parse(Console.ReadLine());
                     Console.WriteLine("Ingrese la hora programada:");
                     int horaTres = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
-                    int produccionTres = int.Parse(Console.ReadLine());
-                    if (duracionDocu >=30 && duracionDocu <= 120)
-                    {
-                        cumpleDuracion=true;
-                    }
                     if (clasificacionTres == 1)
                     {
-                        cumpleHora=true;
+                        cumpleHora = true;
                     }
-                    if (clasificacionTres == 2 && horaTres >= 6 && horaTres <= 22)
+                    else if (clasificacionTres == 2 && horaTres >= 6 && horaTres <= 22)
                     {
-                        cumpleHora=true;
+                        cumpleHora = true;
                     }
-                    if (clasificacionTres == 3 && horaTres >= 22 || horaTres <= 5)
+                    else if (clasificacionTres == 3 && horaTres >= 22 || horaTres <= 5)
                     {
-                        cumpleHora=true;
+                        cumpleHora = true;
                     }
+                    else
+                    {
+                        Console.WriteLine("El horario no es apto para la clasificación.\n");
+                        return;
+                    }
+                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
+                    int produccionTres = int.Parse(Console.ReadLine());
                     if (produccionTres == 1 && clasificacionTres == 3)
                     {
-                        cumpleProduccion = false;
+                        Console.WriteLine("La duracion de la serie no cumple con el reglamento.\n");
+                        return;
                     }
+
                     break;
                 case 4:
                     Console.WriteLine("Ingrese el nombre del evento en vivo");
                     string nombreEvento = Console.ReadLine();
                     Console.WriteLine("Cual es la duración del evento en vivo?");
                     int duracionEvento = int.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la clasificación (1.Todo publico, 2.+13, 3.+18)");
-                    int clasificacionCuatro = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingrese la hora programada:");
-                    int horaCuatro = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
-                    int produccionCuatro = int.Parse(Console.ReadLine());
                     if (duracionEvento >= 30 && duracionEvento <= 120)
                     {
                         cumpleDuracion = true;
                     }
+                    else
+                    {
+                        Console.WriteLine("La duracion del evento en vivo no cumple con el reglamento.\n");
+                        return;
+                    }
+                    Console.WriteLine("ingrese la clasificación (1.Todo publico, 2.+13, 3.+18)");
+                    int clasificacionCuatro = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la hora programada:");
+                    int horaCuatro = int.Parse(Console.ReadLine());
                     if (clasificacionCuatro == 1)
                     {
                         cumpleHora = true;
                     }
-                    if (clasificacionCuatro == 2 && horaCuatro >= 6 && horaCuatro <= 22)
+                    else if (clasificacionCuatro == 2 && horaCuatro >= 6 && horaCuatro <= 22)
                     {
                         cumpleHora = true;
                     }
-                    if (clasificacionCuatro == 3 && horaCuatro >= 22 || horaCuatro <= 5)
+                    else if (clasificacionCuatro == 3 && horaCuatro >= 22 || horaCuatro <= 5)
                     {
                         cumpleHora = true;
                     }
+                    else
+                    {
+                        Console.WriteLine("El horario no es apto para la clasificación.\n");
+                        return;
+                    }
+                    Console.WriteLine("Nivel de Producción (1.bajo, 2.medio, 3.alto)");
+                    int produccionCuatro = int.Parse(Console.ReadLine());
                     if (produccionCuatro == 1 && clasificacionCuatro == 3)
                     {
-                        cumpleProduccion = false;
+                        Console.WriteLine("La duracion de la serie no cumple con el reglamento.\n");
+                        return;
                     }
                     break;
                 default:
@@ -166,11 +212,7 @@
             }
             if (cumpleDuracion && cumpleHora && cumpleProduccion)
             {
-                Console.WriteLine(" Validación Exitosa");
-            }
-            else
-            {
-                Console.WriteLine("Error, no cumple las reglas ");
+                Console.WriteLine(" Validación Exitosa\n");
             }
         }
     } while (option != 5);
